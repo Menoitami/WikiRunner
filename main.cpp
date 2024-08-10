cc8356c5087ebdbc183ddfcd1b454dd9d742078c
Menoitami <lalacfd@gmail.com>
2024-08-10 14:03:13 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "regwindow.h"
+#include "gamewindow.h"
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    GameWindow w;
+    w.show();
+    return a.exec();
+}
