de6d2313d12992c54161ea4f37a8e45047e0aa8d
Menoitami <lalacfd@gmail.com>
2024-08-15 17:38:12 +0300

    add qrc person class json

---
+++ PersonClass/personclass.cpp
@@ -0,0 +1,3 @@
+#include "personclass.h"
+
+PersonClass::PersonClass() {}
---
+++ PersonClass/personclass.h
@@ -0,0 +1,10 @@
+#ifndef PERSONCLASS_H
+#define PERSONCLASS_H
+
+class PersonClass
+{
+public:
+    PersonClass();
+};
+
+#endif // PERSONCLASS_H
